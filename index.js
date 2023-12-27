ae2ab80487fb68dfad23c1dabe1af094cbeb7e4b
Kurkov11 <kurkovsw@icloud.com>
2023-12-27 18:08:49 +0100

    Add a black box div for darkening the main content when mobile-menu is shown

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 document.getElementById('menu-icon').onclick = function(){
     document.getElementById('mobile-menu').style.display = 'flex';
     document.getElementById('stop-scrolling').style.overflow = 'hidden'
+    document.getElementById('black-box').style.display = 'block';
+    document.getElementById('black-box').style.opacity = '0.5';
 }
 document.getElementById('menu-icon-closed').onclick = function(){
     document.getElementById('mobile-menu').style.display = 'none';
     document.getElementById('stop-scrolling').style.overflow = 'scroll';
+    document.getElementById('black-box').style.display = 'none';
 }
